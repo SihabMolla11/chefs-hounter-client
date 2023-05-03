fa0e6c4d40ef4d09ed52f0b6ce8ce9398c4ca86a
SihabMolla11 <sihabuddinmolla11@gamil.com>
2023-05-03 22:46:47 +0600

    fevorite button done

--- src/Components/Recipes/RecipesCard.jsx
+++ src/Components/Recipes/RecipesCard.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const RecipesCard = ({ recipe }) => {
-  // console.log(recipe);
+  const { r_img, name, cooking_method, ingredients, ratting } = recipe;
 
-  const { r_img,name,cooking_method,ingredients,ratting } = recipe;
+  const [buttonDe, setButtonDE] = useState(true)
+
+  const favorite = () => {
+    toast.success("Add in Favorite successfully", {
+      position: "top-center",
+    });
+    setButtonDE(false)
+  };
 
   return (
     <div>
@@ -14,22 +23,34 @@ const RecipesCard = ({ recipe }) => {
         <div className="card-body">
           <h2 className="card-title uppercase">Recipe Name: {name}</h2>
           <p>
-            <strong className="text-lg">Ingredients for create the Recipe:</strong>
-            {
-              ingredients.map(ingredient => <li key={ingredient}>{ingredient}</li>)
-            }
+            <strong className="text-lg">
+              Ingredients for create the Recipe:
+            </strong>
+            {ingredients.map((ingredient) => (
+              <li key={ingredient}>{ingredient}</li>
+            ))}
           </p>
           <p>
-            <strong className="text-lg">Ingredients for create the Recipe:</strong>
-            {
-              cooking_method.map(cooking => <li className="my-2 style-none" key={cooking}>{cooking}</li>)
-            }
+            <strong className="text-lg">
+              Ingredients for create the Recipe:
+            </strong>
+            {cooking_method.map((cooking) => (
+              <li className="my-2 style-none" key={cooking}>
+                {cooking}
+              </li>
+            ))}
           </p>
-
         </div>
-          <div className="justify-end p-4">
-            <button className="my-btn">Add in Favorite</button>
-          </div>
+        <div className="justify-end p-4">
+          <button
+            onClick={favorite}
+            disabled={!buttonDe}
+            className="my-btn"
+          >
+            Add in Favorite
+          </button>
+          <ToastContainer />
+        </div>
       </div>
     </div>
   );
